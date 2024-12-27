ff5fb766d39ffe3d59a392ed5cfc6941a43d7633
HongAn <nguyenhongan.tphcm@gmail.com>
2024-12-27 23:51:38 +0700

    update js order

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -31,8 +31,8 @@ function loadDataTable(status) {
         "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
-            { data: 'name', "width": "25%" },
-            { data: 'phoneNumber', "width": "20%" },
+            { data: 'name', "width": "20%" },
+            { data: 'phoneNumber', "width": "15%" },
             { data: 'applicationUser.email', "width": "20%" },
             { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%" },
@@ -51,7 +51,7 @@ function loadDataTable(status) {
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                      <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
-                    </div>`;
+                    </div>`
                 },
                 "width": "10%"
             }
